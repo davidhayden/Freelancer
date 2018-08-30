6d13844a871be732e48384e41c8318be4d966b0a
davidhayden <davidhayden@users.noreply.github.com>
2018-08-30 18:04:16 -0400

    Adding initial files

---
+++ src/Freelancer.OrchardCore/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Freelancer",
+    Author = "David Hayden",
+    Website = "https://www.davidhayden.me",
+    Version = "2.0.0",
+    Description = "One page Bootstrap portfolio theme for freelancers based on Start Bootstrap template."
+)]
