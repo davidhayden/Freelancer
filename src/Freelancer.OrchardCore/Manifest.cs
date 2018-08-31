d4ffd084c35e0f114e087b172eac583ef6947914
davidhayden <davidhayden@users.noreply.github.com>
2018-08-31 14:44:13 -0400

    Fix website link in manifest

--- src/Freelancer.OrchardCore/Manifest.cs
+++ src/Freelancer.OrchardCore/Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.DisplayManagement.Manifest;
 [assembly: Theme(
     Name = "Freelancer",
     Author = "David Hayden",
-    Website = "https://www.davidhayden.me",
+    Website = "http://www.davidhayden.me",
     Version = "2.0.0",
     Description = "One page Bootstrap portfolio theme for freelancers based on Start Bootstrap template."
 )]
