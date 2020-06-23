2eddaca91727d42fbb3043c52a8f0df568032034
davidhayden <davidhayden@users.noreply.github.com>
2020-06-23 13:55:13 -0400

    Specify https

--- src/Freelancer.OrchardCore/Manifest.cs
+++ src/Freelancer.OrchardCore/Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.DisplayManagement.Manifest;
 [assembly: Theme(
     Name = "Freelancer",
     Author = "David Hayden",
-    Website = "http://www.davidhayden.me",
+    Website = "https://www.davidhayden.me",
     Version = "1.0.0-rc2",
     Description = "One page Bootstrap portfolio theme for freelancers based on Start Bootstrap template.",
     Tags = new[] { "Bootstrap", "Freelancer", "Liquid" }
